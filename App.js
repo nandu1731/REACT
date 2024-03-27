33635b399e2f59a08393ee791a026667601117e4
nandini <harinandinib@turito.com>
2024-03-27 14:28:00 +0530

    added npm scripts

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const para = React.createElement("p", {"id":"para"}, "This is heading");
-const container=React.createElement('div',{'id':'parent'},[para,React.createElement("div",{'id':'child'},"This is child element")]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+
