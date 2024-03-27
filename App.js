d28a39cc86c0b656fb2663248f7f8f375e7d10a7
nandini <harinandinib@turito.com>
2024-03-27 11:21:34 +0530

    installed react module and created build using parcel

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const para = React.createElement("p", {"id":"para"}, "This is heading");
-// console.log(para);
 const container=React.createElement('div',{'id':'parent'},[para,React.createElement("div",{'id':'child'},"This is child element")]);
-console.log(container);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
