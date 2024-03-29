46bff6cb437500fd97be5fc05167c6892ec2b83b
nandini <harinandinib@turito.com>
2024-03-29 22:10:39 +0530

    added header and cards to food app

--- App.js
+++ App.js
@@ -1,25 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxElement = <h1>Hi React</h1>; //JSX code (return React element)
-
-const SubComponent=()=>(
-    <p>This is child component</p>
-)
-
-const MyComponent = () => {
+const Header = () => {
   return (
-    <div className="container">
-      { jsxElement } 
-      <p>
-        Here we are rendering jsx code in component using <span>{`{}`}</span>
-      </p>
-      <SubComponent />
-      <SubComponent></SubComponent>
-      {SubComponent()} 
+    <div className="header">
+      <div className="logo">
+        <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+      </div>
+      <div className="links">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <img src="https://recipes.net/wp-content/uploads/2023/05/air-fryer-chicken-biryani-recipe_6968eb6ab4a5ae22d136dab86c9ea8af.jpeg" />
+      <div className="card-body">
+        <h4>Kritunga Restaurant</h4>
+        <h5>Family Restaurant</h5>
+        <div className="card-pair">
+          <p>4.3 stars </p>
+          <p>40 mins</p>
+        </div>
+        <div className="card-pair">
+          <p>4km</p>
+          <p>Madhapur</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppComponent = () => {
+  return (
+    <div className="root">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MyComponent />);
+root.render(<AppComponent />);
