9c703cbb8bbc50132333b7aa04603d0bc9c18baf
nandini <harinandinib@turito.com>
2024-03-29 10:50:14 +0530

    Components added

--- App.js
+++ App.js
@@ -1,4 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const jsxElement = <h1>Hi React</h1>; //JSX code (return React element)
 
+const SubComponent=()=>(
+    <p>This is child component</p>
+)
+
+const MyComponent = () => {
+  return (
+    <div className="container">
+      { jsxElement } 
+      <p>
+        Here we are rendering jsx code in component using <span>{`{}`}</span>
+      </p>
+      <SubComponent />
+      <SubComponent></SubComponent>
+      {SubComponent()} 
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyComponent />);
