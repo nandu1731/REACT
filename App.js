ca36116bddef34dc435f28ae69b2cdc0af87f73f
nandini <harinandinib@turito.com>
2024-03-31 11:36:10 +0530

    added realtime data to render components dynamically

--- App.js
+++ App.js
@@ -18,20 +18,23 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { info } = props.resInfo;
   return (
     <div className="card">
-      <img src="https://recipes.net/wp-content/uploads/2023/05/air-fryer-chicken-biryani-recipe_6968eb6ab4a5ae22d136dab86c9ea8af.jpeg" />
+      <img
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${info?.cloudinaryImageId}`}
+      />
       <div className="card-body">
-        <h4>Kritunga Restaurant</h4>
-        <h5>Family Restaurant</h5>
+        <h4>{info?.name}</h4>
+        <h5>{info?.cuisines.join(", ")}</h5>
         <div className="card-pair">
-          <p>4.3 stars </p>
-          <p>40 mins</p>
+          <p>{info?.avgRatingString} stars </p>
+          <p>{info?.sla?.slaString}</p>
         </div>
         <div className="card-pair">
-          <p>4km</p>
-          <p>Madhapur</p>
+          <p>{info?.sla?.lastMileTravelString}</p>
+          <p>{info?.areaName}</p>
         </div>
       </div>
     </div>
@@ -43,14 +46,9 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        {restaurantsInfo?.map((restaurant) => (
+          <RestaurantCard key={restaurant?.id} resInfo={restaurant} />
+        ))}
       </div>
     </div>
   );
@@ -67,3 +65,405 @@ const AppComponent = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppComponent />);
+
+const restaurantsInfo = [
+  {
+    info: {
+      id: "10258",
+      name: "Bheemas Grand",
+      cloudinaryImageId: "schjsfazzjcaaywwuap7",
+      locality: "Road No 10",
+      areaName: "Banjara Hills",
+      costForTwo: "₹300 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.3,
+      veg: true,
+      parentId: "1842",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-31 21:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-90680555-fb0c-42f1-a5f5-27ab274aa0fd",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/bheemas-grand-road-no-10-banjara-hills-hyderabad-10258",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "329070",
+      name: "Kamath Hotel",
+      cloudinaryImageId: "wx5qrfddnvfastifgmjy",
+      locality: "Nampally",
+      areaName: "Banjara Hills",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.4,
+      parentId: "19012",
+      avgRatingString: "4.4",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 6,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "6.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-31 22:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-90680555-fb0c-42f1-a5f5-27ab274aa0fd",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kamath-hotel-nampally-banjara-hills-hyderabad-329070",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "579871",
+      name: "Sri Mahalaxmi Reddy Brothers Tiffin Center",
+      cloudinaryImageId: "ixdhdmfecmuei3lufgv0",
+      locality: "Santosh Nagar Colony",
+      areaName: "Mehdipatnam",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian", "Chinese"],
+      avgRating: 4.5,
+      parentId: "347243",
+      avgRatingString: "4.5",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 2.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-31 22:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-90680555-fb0c-42f1-a5f5-27ab274aa0fd",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/sri-mahalaxmi-reddy-brothers-tiffin-center-santosh-nagar-colony-mehdipatnam-hyderabad-579871",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "22986",
+      name: "Lakshman Ki Bandi",
+      cloudinaryImageId: "kecvrvzdmvwkblcy4tuo",
+      locality: "Ghoshamahal",
+      areaName: "Nampally",
+      costForTwo: "₹150 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "19944",
+      avgRatingString: "4.2",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 3.6,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "3.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-31 22:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "UPTO ₹40",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-90680555-fb0c-42f1-a5f5-27ab274aa0fd",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/lakshman-ki-bandi-ghoshamahal-nampally-hyderabad-22986",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "27456",
+      name: "Rambharose - Bhattad Ki Idli",
+      cloudinaryImageId: "568af3b3862bc0bfdf0c30c18da08ff0",
+      locality: "New Malakpet",
+      areaName: "Attapur",
+      costForTwo: "₹300 for two",
+      cuisines: ["South Indian", "Snacks", "Indian"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "166509",
+      avgRatingString: "4.4",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-31 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-90680555-fb0c-42f1-a5f5-27ab274aa0fd",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/rambharose-bhattad-ki-idli-new-malakpet-attapur-hyderabad-27456",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
